9b2a243fe0e90f5102dfdaafb43273310e722d2a
Bogdan MacPro M1 <bogdan@farca.ro>
2022-08-29 14:45:18 +0300

    Initial Commit

---
+++ PiCluster/PiClusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiClusterApp.swift
+//  PiCluster
+//
+//  Created by Bogdan Farca on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PiClusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
